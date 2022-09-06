91b94a03bd1f39ede525b2fb195ba940441a7843
Stephen King <kingze1992@163.com>
2022-09-06 17:55:25 +0800

    Add Bubble Sort

---
+++ solutions/0000_bubble_sort.cc
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+void BubbleSort(vector<int> &nums, int n) {
+  bool swapped;
+  for (int i = 1; i < n; ++i) {
+    swapped = false;
+    for (int j = 1; j < n - i + 1; ++j) {
+      if (nums[j] < nums[j - 1]) {
+        swap(nums[j], nums[j - 1]);
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+}
