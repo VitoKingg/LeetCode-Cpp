92154aa3e025c318c858799cd422e061ecc8275b
Stephen King <kingze1992@163.com>
2022-09-06 17:55:48 +0800

    Add Insertion Sort

---
+++ solutions/0000_insertion_sort.cc
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+void InsertionSort(vector<int> &nums, int n) {
+  for (int i = 0; i < n; ++i) {
+    for (int j = i; j > 0 && nums[j] < nums[j - 1]; --j) {
+      swap(nums[j], nums[j - 1]);
+    }
+  }
+}
